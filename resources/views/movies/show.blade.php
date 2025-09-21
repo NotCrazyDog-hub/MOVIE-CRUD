0b3d231084240afdfc2a05b6ae897919f7678a17
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-09-21 19:33:03 -0300

    view 'show.' modified

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -7,13 +7,13 @@
 </head>
 <body>
     <h1>{{ $movie->title }}</h1>
-    <p style="">{{ $movie->synopsis }}</p>
+    <p>{{ $movie->synopsis }}</p>
     <p>Tem duração de {{ $movie->duration }} minutos</p>
     <p>Lançado em: {{ $movie->release_year }}</p>
     <form action="{{ route('movies.destroy', ['movie' => $movie->id]) }}" method="post">
         @csrf
         @method('delete')
-        <input type="submit" onclick="return confirm('Tem certeza que deseja deletar esse filme?')" value="Deletar">
+        <input type="submit" onclick="return confirm('Tem certeza que deseja deletar esse filme do seu catálogo?')" value="Deletar">
     </form>
 </body>
 </html>
