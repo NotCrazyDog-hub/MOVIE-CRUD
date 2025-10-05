a1c0850d48ba4643d072d47b6207b74714522f12
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-05 10:12:41 -0300

    Remove delete form

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -10,10 +10,5 @@
     <p>{{ $movie->synopsis }}</p>
     <p>Tem duração de {{ $movie->duration }} minutos</p>
     <p>Lançado em: {{ $movie->release_year }}</p>
-    <form action="{{ route('movies.destroy', ['movie' => $movie->id]) }}" method="post">
-        @csrf
-        @method('delete')
-        <input type="submit" onclick="return confirm('Tem certeza que deseja deletar esse filme do seu catálogo?')" value="Deletar">
-    </form>
 </body>
 </html>
