c7bee5267c14ae779eeb5255927155799b1e480a
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-05 11:28:39 -0300

    implement basic template in the editt form

--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -1,24 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar filme</title>
-</head>
-<body>
-    <h1>Editar filme</h1>
-    <form action="{{ route('movies.update', ['movie' => $movie->id]) }}" method="post">
-        @csrf
-        @method('put')
-        <input type="text" name="title" value="{{ $movie->title }}">
-        <br>
-        <textarea name="synopsis">{{ $movie->synopsis }}</textarea>
-        <br>
-        <input type="number" name="duration" value="{{ $movie->duration }}">
-        <br>
-        <input type="number" name="release_year" value="{{ $movie->release_year }}">
-        <br>
-        <input type="submit" value="Editar">
-    </form>
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'Editar Filmes')
+
+@section('content')
+<h1>Editar filme</h1>
+<form class="forms" action="{{ route('movies.update', ['movie' => $movie->id] ) }}" method="post">
+    @csrf
+    @method('put')
+    <label for="title">Título do filme:</label>
+    <input type="text" name="title" value="{{ $movie->title }}">
+
+    <label for="synopsis">Sinopse do filme:</label>
+    <textarea name="synopsis">{{ $movie->synopsis }}</textarea>
+
+    <label for="duration">Duração do filme (em minutos):</label>
+    <input type="number" name="duration" value="{{ $movie->duration }}">
+
+    <label for="release_year">Ano de lançamento:</label>
+    <input type="number" name="release_year" value="{{ $movie->release_year }}">
+
+    <input class="btn-submit" type="submit" value="Atualizar">
+</form>
+@endsection
